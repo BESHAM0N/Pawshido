840a1734203690f0290b85ccd50a5500b6edbc2a
Beshamon <141951306+BESHAM0N@users.noreply.github.com>
2025-02-02 16:52:35 +0300

    Add item and inventory
    Added item with id, name, and inventory with grid

--- Pawshido/Assets/Scripts/Systems/IMoveSystem.cs
+++ Pawshido/Assets/Scripts/Systems/IMoveSystem.cs
@@ -1,7 +1,10 @@
+using UnityEngine;
+
 namespace Systems
 {
     public interface IMoveSystem
     {
-        
+        void Move(Vector3 direction, float speed);
+        void Rotate(Quaternion rotation);
     }
 }
